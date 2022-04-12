d632b5b7f9c1da79a7982636a77151e811228ff9
Julie Napoli <njulie818@gmail.com>
2022-04-12 14:38:51 +0200

    first commit

--- php/db.php
+++ php/db.php
@@ -1,6 +1,6 @@
 <?php
 
-// header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: *");
 header('HTTP/1.1 200');
