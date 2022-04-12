ab43431d17ca99d0fd59e5b97ec95dda4c502e7a
Julie Napoli <njulie818@gmail.com>
2022-04-12 14:54:45 +0200

    first commit

--- php/submit.php
+++ php/submit.php
@@ -1,5 +1,5 @@
 <?php
-header('Access-Control-Allow-Origin: https://memoryfrontjulie.herokuapp.com/');
+header('Access-Control-Allow-Origin: https://memoryfrontjulie.herokuapp.com');
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header('Access-Control-Allow-Credentials: true');
 header('Content-Type: application/json; charset=utf-8');
--- php/user.php
+++ php/user.php
@@ -1,6 +1,6 @@
 <?php
 
-header('Access-Control-Allow-Origin: https://memoryfrontjulie.herokuapp.com/');
+header('Access-Control-Allow-Origin: https://memoryfrontjulie.herokuapp.com');
 header('Access-Control-Allow-Credentials: true');
 header('Content-Type: application/json; charset=utf-8');
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
