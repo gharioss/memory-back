f7c98a5966f0aaf8c1fc0b711d895dfe7066a62d
Julie Napoli <njulie818@gmail.com>
2022-04-12 12:38:45 +0200

    first commit

--- php/db.php
+++ php/db.php
@@ -5,7 +5,7 @@ header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
 try {
-    $pdo = new PDO('mysql:host=localhost;dbname=memory;charset=utf8', 'root', 'root', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION],);
+    $pdo = new PDO('mysql:host=spryrr1myu6oalwl.chr7pe7iynqr.eu-west-1.rds.amazonaws.com;dbname=qx0inh95bxegsxdz;charset=utf8', 'mdwrkz40on3tdyns', 'jqooany1lacnid2a', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION],);
 } catch (Exception $e) {
     die('Erreur : ' . $e->getMessage());
 }
