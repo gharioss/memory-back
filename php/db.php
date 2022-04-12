761f0b4c4897d113fe2d2ad44a274c1f10603a3b
Julie Napoli <njulie818@gmail.com>
2022-04-12 14:03:02 +0200

    first commit

--- php/db.php
+++ php/db.php
@@ -1,8 +1,8 @@
 <?php
 
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Origin: https://memoryfrontjulie.herokuapp.com");
+// header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+// header("Access-Control-Allow-Headers: ");
 
 try {
     $pdo = new PDO('mysql:host=spryrr1myu6oalwl.chr7pe7iynqr.eu-west-1.rds.amazonaws.com;dbname=qx0inh95bxegsxdz;charset=utf8', 'mdwrkz40on3tdyns', 'jqooany1lacnid2a', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION],);
